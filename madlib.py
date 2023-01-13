9eae133aa3222de6134ec14f59ff5a5c1bad0eb5
Barsha Pantha <barshapantha@Barshas-MacBook-Pro.local>
2023-01-13 14:13:05 -0600

    madlib code added

---
+++ madlib.py
@@ -0,0 +1,16 @@
+#create a string that says "subscribe to ____ "
+# youtuber = "Barsha" # some string variable
+# different ways to print
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me excited because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
