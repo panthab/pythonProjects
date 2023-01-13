24735234e4467b430aa2853a70fe98d8f843860c
Barsha Pantha <barshapantha@Barshas-MacBook-Pro.local>
2023-01-13 16:14:03 -0600

    myapp code added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -22,4 +22,4 @@ def is_win(player, opponent):
     if(player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
 
-print(play())
+print(play()) 
